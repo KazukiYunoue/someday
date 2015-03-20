a6582d5f1b5f6caa752bec74dc81ed11818d3882
Kazuki Yunoue <kazuki.yunoue@mail.rakuten.com>
2015-03-20 14:03:33 +0900

    bash

---
+++ someday.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DAYS=./days.list
+YEAR=`date "+%Y"`
+MONTH=`date "+%m"`
+DAY=`date "+%d"`
+
+cat $DAYS | while read line
+do
+  arr=( `echo $line | tr -s '-' ' '`)
+  if [ ${arr[0]} = $YEAR -a ${arr[1]} = $MONTH -a ${arr[2]} = $DAY ]
+  then
+    # TODO: Processing for the day
+    echo 'the day'
+  fi
+done
